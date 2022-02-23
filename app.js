3855d9740d7bd21d9f0439a369e747c81175931e
jayquaza <o4k0h@ugrad.cs.ubc.ca>
2022-02-22 17:15:46 -0800

    updated app.js from master branch

--- app.js
+++ app.js
@@ -3,32 +3,46 @@ const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer(function(req, res) {
-    console.log('about to request html');
-    fs.readFile('index.html', function(error, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        if (error) {
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-        } else {
-            console.log('about to load html');
-            res.write(data);
-        }
-        res.end();
-    });
+    console.log(req.url);
 
-    console.log('about to request css');
-    fs.readFile('css/style.css', function(error, data) {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        if (error) {
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-        } else {
-            console.log('about to load css');
-            res.write(data);
-        }
-        res.end();
-    });
-    console.log('DONE');
+    if (req.url == "/") {
+        fs.readFile('index.html', function(error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                // console.log('about to load html');
+                res.write(data);
+            }
+            res.end();
+        });
+    } else if (req.url == "/css/style.css") {
+        // ASK AIDAN WHY THESE ARE NEEDED DESPITE BEING INCLUDED IN HTML
+        fs.readFile('css/style.css', function(error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                // console.log('about to load css');
+                res.write(data);
+            }
+            res.end();
+        });
+    } else if (req.url == "/js/main.js") {
+        fs.readFile('js/main.js', function(error, data) {
+            res.writeHead(200, { 'Content-Type': 'text/js' });
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                // console.log('about to load js');
+                res.write(data);
+            }
+            res.end();
+        });
+    }
 });
 
 server.listen(port, 'localhost', function(error) {
