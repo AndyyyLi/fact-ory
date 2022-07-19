41834af31d584d36e6f8b36c1c9e4bae7229048a
jayquaza <o4k0h@ugrad.cs.ubc.ca>
2022-07-18 22:15:04 -0700

    Primary functions all work

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.listen(port);
 // middleware
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     const fact = new Fact({ key: 1, fact: "12 is the largest one-syllable number", views: 0, likes: 0 });
@@ -50,21 +51,62 @@ app.use(bodyParser.json());
 // });
 
 // looks for fact in db
-app.post('/search', function (req, res) {
-    Fact.findOne({ key: req.body.key }, function (err, fact) {
+app.get('/api/v1/facts', function (req, res) {
+    let val = req.query.key;
+
+    Fact.findOne({ key: val }, function (err, fact) {
         if (err) res.status(400).send(err);
 
         if (fact) {
-            // display fact
-            console.log("Found! Fact " + fact.key + ": " + fact.fact);
-            res.sendFile('./existingFact.html', {root: __dirname});
+            // display existing fact
+            res.status(201).send(fact);
         } else {
             // show new fact
-            console.log("New fact #" + req.body.key);
-            res.sendFile('./newFact.html', {root: __dirname});
+            res.status(204).send(fact);
         }
     });
 });
+
+// updates an existing fact in db
+app.post('/update', function (req, res) {
+    let liked = req.query.liked;
+    let id = req.query.id;
+
+    Fact.findById(id, function(err, fact) {
+        if (err) res.status(400).send(err);
+
+        if (fact) {
+            if (liked == 'true') fact.likes++;
+            fact.views++;
+            fact.save();
+            
+            res.status(200).send();
+        } else {
+            res.status(404).send();
+        }
+    })
+})
+
+// creates a new fact in db
+app.post('/create', function (req, res) {
+    let key = req.query.key;
+    let factBody = req.body.fact;
+
+    let newFact = new Fact({
+        key: key, 
+        fact: factBody, 
+        views: 0, 
+        likes: 0
+    });
+
+    newFact.save()
+    .then((fact) => {
+        res.status(201).send(fact);
+    })
+    .catch((err) => {
+        res.status(400).send(err);
+    })
+})
 
 // gets webpage, css, and js files
 app.get('/', (req, res) => {
--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var modal = document.getElementById("modalBox");
-var btn = document.getElementById("howItWorks");
+var howItWorksBtn = document.getElementById("howItWorksBtn");
 var close = document.getElementsByClassName("close")[0];
+var keyInput = document.getElementById("keyInput");
 
 // makes modal window appear on click
-btn.onclick = function() {
+howItWorksBtn.onclick = function() {
     modal.style.display = "block";
 }
 
@@ -19,14 +20,212 @@ window.onclick = function(event) {
     }
 }
 
-function showExistingFact() {
-    var existingFact = document.getElementById("existingFact");
+// allows user to search for fact by pressing enter instead of clicking button
+keyInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("searchBtn").click();
+    }
+})
 
-    existingFact.style.display = "block";
+// helper to remove input box from search screen
+function removeInputBox() {
+    let inputBox = document.getElementById("inputBox");
+
+    inputBox.style.display = "none";
 }
 
-function showNewFact() {
-    var newFact = document.getElementById("newFact");
+// helper to change title display from search screen
+function shrinkTitle() {
+    let pretitle = document.getElementById("pretitle");
+    let title = document.getElementById("title");
+
+    pretitle.style.display = "none";
+    title.style.fontSize = "40px";
+}
+
+// removes body elements of existingFact div
+function removeExistingFactElements() {
+    document.getElementById("existingFactHeader").remove();
+    document.getElementById("factBody").remove();
+    document.getElementById("views").remove();
+    document.getElementById("likes").remove();
+}
+
+// removes body elements of newFact div
+function removeNewFactElements() {
+    document.getElementById("newFactHeader").remove();
+    document.getElementById("newFactSubheader").remove();
+    document.getElementById("factInput").remove();
+}
+
+// changes screen to show a new search screen for user to input number (NOT STARTING SCREEN)
+function newSearchScreen(justUpdated) {
+    if (justUpdated) {
+        removeExistingFactElements();
+        document.getElementById("existingFact").style.display = "none";
+    } else {
+        removeNewFactElements();
+        document.getElementById("newFact").style.display = "none";
+    }
+
+    let inputBox = document.getElementById("inputBox");
+    inputBox.style.display = "block";
+    inputBox.style.marginTop = "175px";
+
+    // clears input field
+    document.getElementById("keyInput").value = "";
+}
+
+// fetches for requested fact from server based on user input
+function search() {
+    var searchVal = parseInt(document.getElementById("keyInput").value);
+
+    // defaults blank field as 0
+    if (searchVal == "") searchVal = 0;
+
+    fetch('/api/v1/facts?key=' + searchVal)
+    .then((response) => {
+        if (response.status == 201) return response.json();
+    })
+    .then((fact) => {
+        if (fact) {
+            factSpace.showExistingFact(fact);
+        } else {
+            factSpace.showNewFact(searchVal);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+// module pattern used to keep track of current fact ID or new fact key to update db after user finishes with fact
+var factSpace = function() {
+    var currentId = null;
+    var currentKey = null;
+
+    // displays an existing fact and allows user to like the fact which would update the fact in the db
+    // NOTE: views MUST be incremented every time an existing fact is shown
+    function showExistingFact(fact) {
+        currentId = fact._id;
+        currentKey = fact.key;
+
+        var existingFact = document.getElementById("existingFact");
+        var factDetails = document.getElementById("factDetails");
+        
+        const header = document.createElement("h1");
+        header.textContent = "Fact " + fact.key;
+        header.id = "existingFactHeader";
+        existingFact.insertBefore(header, factDetails);
+
+        const body = document.createElement("p");
+        body.textContent = fact.fact;
+        body.id = "factBody"
+        existingFact.insertBefore(body, factDetails);
+
+        const views = document.createElement("h6");
+        views.textContent = fact.views  + " user(s) have read this fact before you."
+        views.id = "views";
+        factDetails.appendChild(views);
+
+        const likes = document.createElement("h6");
+        likes.textContent = fact.likes + " enjoyed learning about it!"
+        likes.id = "likes";
+        factDetails.appendChild(likes);
+
+        removeInputBox();
+        shrinkTitle();
+
+        existingFact.style.display = "block";
+    }
+
+    // displays an input box that allows user to input their own fact which would create the fact in the db
+    function showNewFact(key) {
+        currentKey = key;
+
+        var newFact = document.getElementById("newFact");
+        var factOptions = document.getElementsByClassName("factOptions")[1];
+
+        const header = document.createElement("h1");
+        header.textContent = "Fact " + key;
+        header.id = "newFactHeader"
+        newFact.insertBefore(header, factOptions);
+
+        const subheader = document.createElement("h3");
+        subheader.textContent = "No fact here, add one of your own!";
+        subheader.id = "newFactSubheader"
+        newFact.insertBefore(subheader, factOptions);
+
+        const factInput = document.createElement("textarea");
+        factInput.placeholder = "Something something interesting...";
+        factInput.maxLength = "500";
+        factInput.id = "factInput";
+        newFact.insertBefore(factInput, factOptions);
+
+        removeInputBox();
+        shrinkTitle();
+
+        newFact.style.display = "block";
+    }
+
+    // updates current fact in server based on id and userLiked
+    function updateFact(userLiked) {
+        if (currentId == null) {
+            console.log("Error, no fact id present");
+            return;
+        }
+
+        fetch('/update?liked=' + userLiked + '&id=' + currentId, { method: 'POST' })
+        .then((res) => {
+            if (res.status == 404) {
+                console.log("Something went wrong with accessing DB");
+                return;
+            }
+
+            if (userLiked) {
+                alert("Factastic! I'm sure fact " + currentKey + " likes you too! :)");
+            }
     
-    newFact.style.display = "block";
-}
+            currentId = null;
+            currentKey = null;
+
+            newSearchScreen(true);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    // creates and inserts a new fact into db based on key
+    function addFact() {
+        var factInput = document.getElementById("factInput").value;
+        var factData = { fact: factInput }
+
+        fetch('/create?key=' + currentKey, {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(factData)
+        })
+        .then((res) => {
+            if (res.status == 400) {
+                console.log("Something went wrong with accessing DB");
+            } else {
+                alert("Successfully added fact " + currentKey + ", thank you for your contribution!");
+
+                currentKey = null;
+            }
+
+            newSearchScreen(false);
+        })
+    }
+
+    return {
+        showExistingFact:showExistingFact,
+        showNewFact:showNewFact,
+        update:updateFact,
+        addFact:addFact
+    }
+}();
+
